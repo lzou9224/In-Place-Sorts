44d6a641978210ccde8a2ce4e456e528d41806f3
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-01-17 09:16:08 -0500

    s

--- In-Place Sorts/src/ArrayMethods1.java
+++ In-Place Sorts/src/ArrayMethods1.java
@@ -1,4 +1,26 @@
 
 public class ArrayMethods1 {
 
+	//Bubble sort iterates through the list, swapping any out of order elements. We keep iterating until no swaps are required
+	
+	public static void bubbleSort(String [] list1)
+	{
+		
+	}
+	
+	//Insertion takes each element from the array, and adds it to the front of the array in the correct order. 
+	//We need to keep track of where we are inserting elements as we sort.
+	public static void insertionSort(int[] list1)
+	{
+		
+	}
+	
+	//Selection sort finds the lowest element and moves it to the front of the array. 
+	//Then searches the rest of the array for the next element, and repeats.
+	public static void selectionSort(double [] list1)
+	{
+		
+	}
+	
+
 }
