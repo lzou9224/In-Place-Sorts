39225c5681ed2a9e36eb85fcd071e27243750577
Lingli <Lingli@Anesthesia>
2018-01-20 17:51:52 -0500

    :>

--- In-Place Sorts/src/ArrayMethods1.java
+++ In-Place Sorts/src/ArrayMethods1.java
@@ -4,7 +4,17 @@ public class ArrayMethods1 {
 
 	public static void main(String[] args)
 	{
-		String list1 []= {"HAHAHA, BABABA, HEHEHE"};
+		String list3[]=;
+		bubbleSort(list1);
+		
+		int list1[]= {1,6,7,9,0,5};		
+		insertionSort(list1);
+
+		double list2[]= {1,6,7,9,0,5};
+		selectionSort(list2);
+
+		System.out.println();
+		
 	}	
 	
 	//Bubble sort iterates through the list, swapping any out of order elements. We keep iterating until no swaps are required
@@ -30,28 +40,29 @@ public class ArrayMethods1 {
 	//We need to keep track of where we are inserting elements as we sort.
 	public static void insertionSort(int[] list1)
 	{
-		
+		for(int outside=1; outside<list1.length; outside++)
+		{
+			
+		}
 	}
 	
 	//Selection sort finds the lowest element and moves it to the front of the array. 
 	//Then searches the rest of the array for the next element, and repeats.
 	public static void selectionSort(double [] list1)
 	{
-		int temp;
+		double temp=0.0;
 		boolean swap;
 		while(swap>1)
 		{
 			for(int outside=0; outside<list1.length; outside++)
 			{
-				for(int inside=1; inside>0; inside++)
+				for(int inside=1+outside; inside<list1.length; inside++)
 				{
-					if(list1[inside]<list1[inside-1])
+					if(list1[inside]>list1[outside])
 					{
-						list1[inside]=list1[inside-1];
-						list1[inside-1]=temp;
+						list1[inside]=list1[outside];
 					}
 				}
-
 			}
 		}
 	}
