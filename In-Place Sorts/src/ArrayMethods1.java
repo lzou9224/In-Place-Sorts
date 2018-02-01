5cce0cf67d6359439743c2b3d998dea0b4305996
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-02-01 08:59:53 -0500

    a

--- In-Place Sorts/src/ArrayMethods1.java
+++ In-Place Sorts/src/ArrayMethods1.java
@@ -1,5 +1,6 @@
 //Lingli Zou
-//Lab 3.1 In Place Sort
+//2/1/2017
+//Lab 3.1: In Place Sort
 public class ArrayMethods1 {
 
 	public static void main(String[] args)
@@ -17,27 +18,54 @@ public class ArrayMethods1 {
 		
 	}	
 	
-	//Bubble sort iterates through the list, swapping any out of order elements. We keep iterating until no swaps are required
+	//Bubble sort iterates through the list, swapping any out of order elements. 
+	//We keep iterating until no swaps are required
 	public static void bubbleSort(String [] list1)
 	{
 		
 	    String temp;
-	    
-	    for(int inside=0; inside<list1.length; inside++)
-	    {
-	    	for(int outside=0; outside>list1.length-1; outside++)
-			{
-	    		if(list1[inside].compareTo(list1[inside+1])>0)
+	    int swap=1000;
+	    while(swap>0) {
+	    	swap=0;
+	    	for(int inside=0; inside<list1.length; inside++)
+	    	{
+	    		for(int outside=0; outside>list1.length-1; outside++)
 	    		{
-	    			temp=list1[inside];
-	    			list1[inside]=list1[inside+1];
-	    			list1[inside+1]=temp;
-				}
-				
+	    			if(list1[inside].compareTo(list1[inside+1])>0)
+	    			{
+	    				temp=list1[inside];
+	    				list1[inside]=list1[inside+1];
+	    				list1[inside+1]=temp;
+	    			}
+	    		}
 			}
 		}
 	}
 	
+	//Selection sort finds the lowest element and moves it to the front of the array. 
+	//Then searches the rest of the array for the next element, and repeats.
+	public static void selectionSort(double [] list1)
+	{
+		double temp= 0.0;
+		int smallest=0;
+	
+		for(int outside=0; outside<list1.length-1; outside++)
+		{
+			outside=smallest;
+			for(int inside=1; inside<list1.length-1; inside++)
+			{
+				if(list1[inside]<list1[smallest])
+				{
+					smallest = inside;
+				}
+			}
+			temp=list1[smallest];
+			list1[smallest]=list1[outside];
+			list1[outside]=temp;
+		}
+	}	
+
+	        
 	//Insertion takes each element from the array, and adds it to the front of the array in the correct order. 
 	//We need to keep track of where we are inserting elements as we sort.
 	public static void insertionSort(int[] list1)
@@ -48,28 +76,7 @@ public class ArrayMethods1 {
 		}
 	}
 	
-	//Selection sort finds the lowest element and moves it to the front of the array. 
-	//Then searches the rest of the array for the next element, and repeats.
-	public static void selectionSort(double [] list1)
-	{
-		double temp= 0.0;
-		int smallest=0;
-		int swap;
-		while(swap>1)
-		{
-			for(int outside=0; outside<list1.length-1; outside++)
-			{
-				outside=smallest;
-				for(int inside=1; inside<list1.length-1; inside++)
-				{
-					if(list1[inside]<list1[smallest])
-					{
-						
-					}
-				}
-			}
-		}
-	}
+
 	
 
 }
